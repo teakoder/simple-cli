8b0f5c343337c7cf6f9dfa4393ecef8d1039dd03
Jen You <4979198+teakoder@users.noreply.github.com>
2021-06-07 11:25:54 +0000

    update license header

--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+Copyright © 2021 TeaKoder <tea@koder.dev>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+
 	"github.com/spf13/cobra"
 
 	homedir "github.com/mitchellh/go-homedir"
