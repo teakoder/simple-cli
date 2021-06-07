b0923d0cad340133744b7840639218bf41705520
Jen You <4979198+teakoder@users.noreply.github.com>
2021-06-07 11:30:18 +0000

    update short & long description

--- cmd/root.go
+++ cmd/root.go
@@ -30,9 +30,9 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "newCLI",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "A super simple CLI with baked-in good practices",
+	Long: `You can use this starter kit to get you up-to-speed in
+your CLI tool development. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
